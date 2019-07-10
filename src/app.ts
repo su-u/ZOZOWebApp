0de08143e36357051061491178e6ba2e10a9359a
すー <aaaiiiuuu35@gmail.com>
2019-07-10 22:24:52 +0900

    build

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+// classを指定してクラス化
+class Main {
+    // コンストラクター
+    constructor() {
+        console.log("Hello! Node.js × TypeScript from Class");
+    }
+}
+
+// Mainクラスのインスタンスを作る
+const main = new Main();
