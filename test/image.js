1c821f735dd4e860ab6128ebf2e142efc66be285
すー <aaaiiiuuu35@gmail.com>
2019-07-11 19:43:40 +0900

    Create image.js

---
+++ test/image.js
@@ -0,0 +1,8 @@
+const imgChanger = require('imageChanger')
+const assert = require('assert')
+
+describe('ImageChangerTest', function() {
+  it('color', function() {
+      imageChanger(150,154,165)
+  })
+})
