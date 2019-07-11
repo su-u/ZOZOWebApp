f18d8a6f2c0d275f7e238cbaf21d28458b64f09b
すー <aaaiiiuuu35@gmail.com>
2019-07-11 19:37:35 +0900

    Update image_changer.js

--- image_changer.js
+++ image_changer.js
@@ -1,47 +1,40 @@
-var fs = require('fs');
+module.exports = (function (color_r, color_g, color_b, color_a = 1) {
+    var fs = require('fs');
 
-var canvas_saver = require('./canvas_saver.js');
+    var canvas_saver = require('./canvas_saver.js');
 
-// node-canvas
-var Canvas = require('canvas'),
-    Image = Canvas.Image;
+    // node-canvas
+    var Canvas = require('canvas'),
+        Image = Canvas.Image;
 
-fs.readFile(__dirname + '/huku.PNG', function (err, data) {
-    if (err) throw err;
+    fs.readFile(__dirname + '/huku.PNG', function (err, data) {
+        if (err) throw err;
 
-    // データをcanvasのcontextに設定
-    var img = new Image;
-    img.src = data;
-    var canvas = new Canvas.Canvas(img.width, img.height);
-    var ctx = canvas.getContext('2d');
-    ctx.drawImage(img, 0, 0, img.width, img.height);
+        // データをcanvasのcontextに設定
+        var img = new Image;
+        img.src = data;
+        var canvas = new Canvas.Canvas(img.width, img.height);
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0, img.width, img.height);
 
-    var imagedata = ctx.getImageData(0, 0, img.width, img.height);
+        var imagedata = ctx.getImageData(0, 0, img.width, img.height);
 
-    // 画像加工(擬似モノクロ化)
-    for (var y = 0; y < imagedata.height; y++) {
-        for (var x = 0; x < imagedata.width; x++) {
-            var index = (y * imagedata.width + x) * 4;
-            imagedata.data[index]       = 154;
-            imagedata.data[index + 1]   = 219;
-            imagedata.data[index + 2]   = 13;
-            // imagedata.data[index+3]; // alpha
+        // 画像加工(擬似モノクロ化)
+        for (var y = 0; y < imagedata.height; y++) {
+            for (var x = 0; x < imagedata.width; x++) {
+                var index = (y * imagedata.width + x) * 4;
+                imagedata.data[index]       = color_r;
+                imagedata.data[index + 1]   = color_g;
+                imagedata.data[index + 2]   = color_b;
+                // imagedata.data[index+3] = color_a;
+            }
         }
-    }
 
-    ctx.putImageData(imagedata, 0, 0);
+        ctx.putImageData(imagedata, 0, 0);
 
-    // データを保存
-    canvas_saver.save(canvas, "huku2.png", function () {
-        console.log("画像保存完了したよ!!");
+        // データを保存
+        canvas_saver.save(canvas, "huku2.png", function () {
+            console.log("画像保存完了したよ!!");
+        });
     });
-    return {
-        save: function (canvas, name, callback) {
-            decode_and_copy(
-                canvas_to_base64(canvas),
-                name,
-                callback
-            );
-        }
-    }
 });
