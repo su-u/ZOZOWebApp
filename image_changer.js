0d9d88900af11639335dbccffa122415bc415365
すー <aaaiiiuuu35@gmail.com>
2019-07-11 23:42:10 +0900

    Update image_changer.js

--- image_changer.js
+++ image_changer.js
@@ -1,16 +1,19 @@
-module.exports.imageChanger = (function (color_r, color_g, color_b, color_a = 1) {
+module.exports.imageChanger = (function (filename, color_r, color_g, color_b, color_a = 1) {
     const fs = require('fs');
-
     const canvas_saver = require('./canvas_saver.js');
-
-    // node-canvas
     const Canvas = require('canvas'),
         Image = Canvas.Image;
 
-    fs.readFile(__dirname + '/huku.PNG', function (err, data) {
+    const checkColor = function (color){
+        return color >= 0 && color < 255;
+    }
+    if(!checkColor(color_r) || !checkColor(color_g) || !checkColor(color_b)){
+        return -1;
+    }
+
+    fs.readFile(__dirname + finename, function (err, data) {
         if (err) throw err;
 
-        // データをcanvasのcontextに設定
         var img = new Image;
         img.src = data;
         var canvas = new Canvas.Canvas(img.width, img.height);
@@ -19,7 +22,6 @@ module.exports.imageChanger = (function (color_r, color_g, color_b, color_a = 1)
 
         var imagedata = ctx.getImageData(0, 0, img.width, img.height);
 
-        // 画像加工(擬似モノクロ化)
         for (var y = 0; y < imagedata.height; y++) {
             for (var x = 0; x < imagedata.width; x++) {
                 var index = (y * imagedata.width + x) * 4;
@@ -34,7 +36,7 @@ module.exports.imageChanger = (function (color_r, color_g, color_b, color_a = 1)
 
         // データを保存
         canvas_saver.save(canvas, "huku2.png", function () {
-            console.log("画像保存完了したよ!!");
+            console.log("img saved");
         });
     });
 });
