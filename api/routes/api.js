bc83f1618822e913878e634b05a6e197e999dc5c
すー <aaaiiiuuu35@gmail.com>
2019-07-12 19:06:40 +0900

    Update api.js

--- api/routes/api.js
+++ api/routes/api.js
@@ -11,6 +11,27 @@ router.get('/img', function (req, res, next) {
     }
 });
 
+router.get('/test', function (req, res, next) {
+    const imgChanger = require('../../image_changer');
+
+    let type = "";
+    let r;
+    // NAMEパラメタが空でなければ画面に表示
+    if (req.query.type) {
+        type = req.query.type;
+    }
+    if (req.query.r) {
+        r = req.query.r;
+    }
+
+    const [status, result] = imgChanger.imageChanger('./huku.PNG', r, 242, 235);
+    if (status == -1) {
+        res.send(result)
+    } else {
+        res.send('<img src="data:image/png;base64,' + result + '">');
+    }
+});
+
 
 
 module.exports = router;
