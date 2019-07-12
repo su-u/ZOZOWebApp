5067a157218535659a0bbc04d27004fdaeb96503
すー <aaaiiiuuu35@gmail.com>
2019-07-13 00:39:27 +0900

    Update api.js

--- api/routes/api.js
+++ api/routes/api.js
@@ -6,8 +6,9 @@ router.get('/img', function (req, res, next) {
     const [status, result] = imgChanger.imageChanger('./huku.PNG', 245, 242, 235);
     if (status == -1) {
         res.json({
-            status: "error",
-            data: result
+            status: 400,
+            message: result,
+            response: ""
         });
     } else {
         res.send('<img src="data:image/png;base64,' + result + '">');
@@ -19,29 +20,44 @@ router.get('/test', function (req, res, next) {
 
     let type = "";
     let color = "";
-    // NAMEパラメタが空でなければ画面に表示
+
+    //空チェック
     if (req.query.type) {
         type = req.query.type;
     } else {
         res.json({
-            status: "error",
-            message: "type is empty"
+            status: 400,
+            message: "type is empty.",
+            response: ""
         });
     }
     if (req.query.color) {
         color = req.query.color;
     } else {
         res.json({
-            status: "error",
-            message: "color is empty"
+            status: 400,
+            message: "color is empt.",
+            response: ""
         });
     }
 
-    const [status, result] = imgChanger.imageChanger('./huku.PNG', r, 242, 235);
+    //値チェック
+    if (color.length != 6) {
+        res.json({
+            status: 400,
+            message: "The number of characters in the color value is not 6.",
+            response: ""
+        });
+    }
+
+
+
+    const [status, result] = imgChanger.imageChanger('./huku.PNG', 120, 242, 235);
     if (status == -1) {
         res.json({
-            status: "error",
-            data: result
+            status: 400,
+            message: result,
+            response: ""
         });
     } else {
         res.send('<img src="data:image/png;base64,' + result + '">');
