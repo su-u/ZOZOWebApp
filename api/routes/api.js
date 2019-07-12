e7051cf2d26509c74df1b44c2e6471849151f050
すー <aaaiiiuuu35@gmail.com>
2019-07-12 18:23:03 +0900

    Update: 簡易エラー処理

--- api/routes/api.js
+++ api/routes/api.js
@@ -3,8 +3,12 @@ var router = express.Router();
 
 router.get('/img', function (req, res, next) {
     const imgChanger = require('../../image_changer');
-    const img = imgChanger.imageChanger('./huku.PNG', 245, 242, 165);
-    res.send('<img src="data:image/png;base64,' + img + '">');
+    const img = imgChanger.imageChanger('./huku.PNG', 245, 242, 235);
+    if (img == -1) {
+        res.send("img error")
+    } else {
+        res.send('<img src="data:image/png;base64,' + img + '">');
+    }
 });
 
 
