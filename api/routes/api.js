e03f078c7f7e31b30f52c768f510e0e886a20853
すー <s1732087AP@s.chibakoudai.jp>
2019-07-12 14:31:33 +0900

    Update api.js

--- api/routes/api.js
+++ api/routes/api.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/img', function (req, res, next) {
-  const imgChanger = require('../../image_changer.js');
-  const img = imgChanger.imageChanger('../../huku.png',211,154,165);
+  const imgChanger = require('../../image_changer');
+//   const img = imgChanger.imageChanger('../../huku.PNG',211,154,165);
   res.send("img");
 });
 
