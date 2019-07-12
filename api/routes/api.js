73aa66887f42e6c93cee2032074e4b3981d0a0b9
すー <aaaiiiuuu35@gmail.com>
2019-07-13 00:30:38 +0900

    Update api.js

--- api/routes/api.js
+++ api/routes/api.js
@@ -6,9 +6,9 @@ router.get('/img', function (req, res, next) {
     const [status, result] = imgChanger.imageChanger('./huku.PNG', 245, 242, 235);
     if (status == -1) {
         res.json({
-            message: status,
+            status: "error",
             data: result
-        })
+        });
     } else {
         res.send('<img src="data:image/png;base64,' + result + '">');
     }
@@ -18,18 +18,31 @@ router.get('/test', function (req, res, next) {
     const imgChanger = require('../../image_changer');
 
     let type = "";
-    let r;
+    let color = "";
     // NAMEパラメタが空でなければ画面に表示
     if (req.query.type) {
         type = req.query.type;
+    } else {
+        res.json({
+            status: "error",
+            message: "type is empty"
+        });
     }
-    if (req.query.r) {
-        r = req.query.r;
+    if (req.query.color) {
+        color = req.query.color;
+    } else {
+        res.json({
+            status: "error",
+            message: "color is empty"
+        });
     }
 
     const [status, result] = imgChanger.imageChanger('./huku.PNG', r, 242, 235);
     if (status == -1) {
-        res.send(result)
+        res.json({
+            status: "error",
+            data: result
+        });
     } else {
         res.send('<img src="data:image/png;base64,' + result + '">');
     }
