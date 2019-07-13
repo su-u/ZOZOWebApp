3e3d66ec898d426713fd1b652736c7ed7f8494d0
すー <aaaiiiuuu35@gmail.com>
2019-07-13 19:12:56 +0900

    Update callapi.js

--- test/callapi.js
+++ test/callapi.js
@@ -1,3 +1,19 @@
 const apitest = require('./apiTest');
+const assert = require('assert');
+const request = require('supertest');
+const app = require('../api/app');
+const expect = require('expect');
 
 apitest('/api/v1/test');
+apitest('/api/v1/tops');
+apitest('/api/v1/bottoms');
+apitest('/api/v1/shoes');
+
+describe('Calltest [/]', function (done) {
+    it('index', function (done) {
+        request(app)
+            .get('/')
+            .expect(200)
+            .end(done);
+    });
+});
