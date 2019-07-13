f95369db09d1a9e0fe947b68c5661da23103f679
すー <aaaiiiuuu35@gmail.com>
2019-07-13 18:52:42 +0900

    Update tops.js

---
+++ api/routes/tops.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const checkColorValue = require('../app/checkColorValue');
+const checkTypeValue = require('../app/checkTypeValue');
+
+router.get('/tops', function (req, res, next) {
+    const imgChanger = require('../app/image_changer');
+
+    const [typeStatus, typeMessage, type] = checkTypeValue(req.query.type);
+    if (typeStatus == -1) {
+        res.status(400).json({
+            status: 400,
+            message: typeMessage,
+            response: ''
+        });
+        return;
+    }
+
+    const [colorStatus, colorMessage, r, g, b] = checkColorValue(req.query.color);
+    if (colorStatus == -1) {
+        res.status(400).json({
+            status: 400,
+            message: colorMessage,
+            response: ''
+        });
+        return;
+    }
+
+    const [imgSstatus, result] = imgChanger.imageChanger('./IMG_0771_c.PNG', r, g, b);
+    if (imgSstatus == -1) {
+        res.status(400).json({
+            status: 400,
+            message: result,
+            response: ''
+        });
+        return;
+    } else {
+        res.status(200).send('<img src="data: image / png; base64, ' + result + '">');
+        return;
+    }
+});
+
+module.exports = router;
