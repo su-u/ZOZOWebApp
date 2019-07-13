a5d362341f24593ca0defa2e1890f3b6d25ec8eb
すー <aaaiiiuuu35@gmail.com>
2019-07-13 18:53:37 +0900

    Update shoes.js

---
+++ api/routes/shoes.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const checkColorValue = require('../app/checkColorValue');
+const checkTypeValue = require('../app/checkTypeValue');
+
+router.get('/shoes', function (req, res, next) {
+    const imgChanger = require('../app/image_changer');
+
+    const [typeStatus, typeMessage, type] = checkTypeValue(req.query.type);
+    if (typeStatus == -1) {
+        res.status(400).json({
+            status: 400,
+            message: typeMessage,
+            response: ''
+        });
+        return;
+    }
+
+    const [colorStatus, colorMessage, r, g, b] = checkColorValue(req.query.color);
+    if (colorStatus == -1) {
+        res.status(400).json({
+            status: 400,
+            message: colorMessage,
+            response: ''
+        });
+        return;
+    }
+
+    const [imgSstatus, result] = imgChanger.imageChanger('./IMG_0773_c.PNG', r, g, b);
+    if (imgSstatus == -1) {
+        res.status(400).json({
+            status: 400,
+            message: result,
+            response: ''
+        });
+        return;
+    } else {
+        res.status(200).send('<img src="data: image / png; base64, ' + result + '">');
+        return;
+    }
+});
+
+module.exports = router;
