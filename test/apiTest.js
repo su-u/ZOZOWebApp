94b47c973290f110a71d5e4fa8e56cef08cfdd47
すー <aaaiiiuuu35@gmail.com>
2019-07-13 12:25:21 +0900

    Add: color api test

--- test/apiTest.js
+++ test/apiTest.js
@@ -10,18 +10,44 @@ describe('ApiTest', function (done) {
         request(app).get('/')
             .expect(200);
     });
-    it('ApiimgTest', function (done) {
+    it('Call api', function (done) {
         request(app).get('/api/v1/img')
             .expect(200)
             .end(done);
 
     });
-    it('Testtest', function (done) {
+    it('Non value test', function (done) {
         request(app)
             .get('/api/v1/test')
             .expect(400, {
                 status: 400,
                 message: 'type is empty.',
+                response: ''
+            }, done);
+    });
+    it('6 characters test', function (done) {
+        request(app)
+            .get('/api/v1/test?color=FFaFFFFd&type=testtype')
+            .expect(400, {
+                status: 400,
+                message: 'The number of characters in the color value is not 6.',
+                response: ''
+            }, done);
+    });
+
+    it('Color value test', function (done) {
+        request(app)
+            .get('/api/v1/test?color=FFFFFF&type=testtype')
+            .expect(200)
+            .end(done);
+    });
+
+    it('R is out of value test', function (done) {
+        request(app)
+            .get('/api/v1/test?color=-1FFFF&type=testtype')
+            .expect(400, {
+                status: 400,
+                message: 'The value of R is out of range.',
                 response: ''
             }, done);
     });
