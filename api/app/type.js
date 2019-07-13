54007a949ebfdb049a94cc7d5ddb1ed49312532b
すー <aaaiiiuuu35@gmail.com>
2019-07-14 01:05:47 +0900

    Update type.js

--- api/app/type.js
+++ api/app/type.js
@@ -1,12 +1,23 @@
-const topsImg = (type) => {
+const getTopsImg = (type) => {
+    const file = ['./img/IMG_0771_c.PNG', './img/IMG_0776_c.PNG', './img/IMG_0769_c.PNG'];
+
+    const path = file[type];
+    if (path == undefined) {
+        return -1;
+    }
+    return path;
+}
+
+
+const getBottomsImg = (type) => {
+
 
 }
 
-const bottomsImg = (type) => {
-
-
-}
-
-const shoesImg = (type) => {
+const getShoesImg = (type) => {
 
 }
+
+module.exports.getTopsImg = getTopsImg;
+module.exports.getBottomsImg = getBottomsImg;
+module.exports.getShoesImg = getShoesImg;
