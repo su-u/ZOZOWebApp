2922a136b559f068104260378832f3b44a978c57
すー <aaaiiiuuu35@gmail.com>
2019-07-14 23:38:47 +0900

    Update: call api test

--- api/app.js
+++ api/app.js
@@ -9,6 +9,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -26,7 +32,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -46,8 +51,5 @@ app.use(function (err, req, res, next) {
     res.render('error');
 });
 
-// var server = app.listen(3000, function () {
-//     console.log("Node.js is listening to PORT:" + server.address().port);
-// });
 
 module.exports = app;
