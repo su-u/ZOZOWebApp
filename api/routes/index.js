660685e7f627f9459f0b80bfd0ea3e61f249b30d
すー <aaaiiiuuu35@gmail.com>
2019-07-16 20:40:43 +0900

    Update: / redirect index.html

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.redirect('/index.html');
 });
 
 module.exports = router;
