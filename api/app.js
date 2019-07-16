3d6457c94d68bb82d76e65a8042c34cc87fad7be
すー <aaaiiiuuu35@gmail.com>
2019-07-16 20:40:56 +0900

    Update app.js

--- api/app.js
+++ api/app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -19,13 +18,11 @@ app.use(function (req, res, next) {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-app.use('/api/v1', require('./routes/api.js'));
 app.use('/api/v1', require('./routes/tops.js'));
 app.use('/api/v1', require('./routes/bottoms.js'));
 app.use('/api/v1', require('./routes/shoes.js'));
+app.use('/', indexRouter);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -40,14 +37,8 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
     console.error(err.stack);
-
-    // render the error page
-    res.status(err.status || 5000);
     res.render('error');
 });
 
