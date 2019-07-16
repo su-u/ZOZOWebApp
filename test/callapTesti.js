e8d1e0643a9b68556f0759e3fbc8d8badaf41809
すー <aaaiiiuuu35@gmail.com>
2019-07-16 20:40:12 +0900

    Add: 404 test

--- test/callapTesti.js
+++ test/callapTesti.js
@@ -4,16 +4,27 @@ const request = require('supertest');
 const app = require('../api/app');
 const expect = require('expect');
 
-apitest('/api/v1/test');
 apitest('/api/v1/tops');
 apitest('/api/v1/bottoms');
 apitest('/api/v1/shoes');
 
-describe('Calltest [/]', function (done) {
-    it('Index', function (done) {
+describe('Calltest', function (done) {
+    it('/', function (done) {
         request(app)
             .get('/')
+            .expect(302)
+            .end(done);
+    });
+    it('/index.html', function (done) {
+        request(app)
+            .get('/index.html')
             .expect(200)
+            .end(done);
+    });
+    it('/aaaaaa', function (done) {
+        request(app)
+            .get('/aaaaaa')
+            .expect(404)
             .end(done);
     });
 });
