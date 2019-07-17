53484d7863a12ae952313c3ed9dca60f22d30164
すー <s1732087AP@s.chibakoudai.jp>
2019-07-17 18:58:25 +0900

    Update: refactoring

--- api/app/type.js
+++ api/app/type.js
@@ -1,15 +1,19 @@
+const CreateResponse = (statusCode, message, path) => {
+    return [statusCode, message, path];
+}
+
 const getTopsImg = (type) => {
     const file = ['./img/IMG_0771_c.PNG', './img/IMG_0776_c.PNG', './img/IMG_0769_c.PNG'];
 
     if (type == null) {
-        return [-1, 'type is empty.', ''];
+        return CreateResponse(-1, 'type is empty.', '');
     }
 
     const path = file[type];
     if (path == undefined) {
-        return [-1, 'type is out of range.', ''];
+        return CreateResponse(-1, 'type is out of range.', '');
     }
-    return [1, '', path];
+    return CreateResponse(1, '', path);
 }
 
 
@@ -17,28 +21,28 @@ const getBottomsImg = (type) => {
     const file = ['./img/IMG_0774_c.PNG', './img/IMG_0775_c.PNG'];
 
     if (type == null) {
-        return [-1, 'type is empty.', ''];
+        return CreateResponse(-1, 'type is empty.', '');
     }
 
     const path = file[type];
     if (path == undefined) {
-        return [-1, 'type is out of range.', ''];
+        return CreateResponse(-1, 'type is out of range.', '');
     }
-    return [1, '', path];
+    return CreateResponse(1, '', path);
 }
 
 const getShoesImg = (type) => {
     const file = ['./img/IMG_0773_c.PNG'];
 
     if (type == null) {
-        return [-1, 'type is empty.', ''];
+        return CreateResponse(-1, 'type is empty.', '');
     }
 
     const path = file[type];
     if (path == undefined) {
-        return [-1, 'type is out of range.', ''];
+        return CreateResponse(-1, 'type is out of range.', '');
     }
-    return [1, '', path];
+    return CreateResponse(1, '', path);
 }
 
 module.exports.getTopsImg = getTopsImg;
