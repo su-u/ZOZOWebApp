a3db442391bf9036950bacb57ef4180ead7c746f
すー <s1732087AP@s.chibakoudai.jp>
2019-07-17 18:49:25 +0900

    Update: document & refactoring

--- api/app/checkColorValue.js
+++ api/app/checkColorValue.js
@@ -1,10 +1,20 @@
+/**
+ * カラーコードを受け取り、r,g,b値を返す。
+ * 問題がある場合は、エラーメッセージを返す。
+ * @param {*} color 処理するカラーコード
+ * @returns [値に対するステータスコード, メッセージ, R値, G値, B値]
+ */
 const checkColorValue = (color) => {
+    const CreateResponse = (statuCode, message, r, g, b) =>{
+        return [statuCode, message, r, g, b];
+    }
+
     if (!color) {
-        return [-1, 'color is empty.', 0, 0, 0];
+        return CreateResponse(-1, 'color is empty.', 0, 0, 0);
     }
 
     if (color.length != 6) {
-        return [-1, 'The number of characters in the color value is not 6.', 0, 0, 0];
+        return CreateResponse(-1, 'The number of characters in the color value is not 6.', 0, 0, 0);
     }
 
     const segments = color.match(/.{2}/g);
@@ -13,26 +23,26 @@ const checkColorValue = (color) => {
     const b = parseInt(segments[2], 16);
 
     if (isNaN(r)) {
-        return [-1, 'R is NaN.', 0, 0, 0];
+        return CreateResponse(-1, 'R is NaN.', 0, 0, 0);
     }
     if (r < 0 || r > 255) {
-        return [-1, 'The value of R is out of range.', 0, 0, 0];
+        return CreateResponse(-1, 'The value of R is out of range.', 0, 0, 0);
     }
     if (isNaN(g)) {
-        return [-1, 'G is NaN.', 0, 0, 0];
+        return CreateResponse(-1, 'G is NaN.', 0, 0, 0);
     }
     if (g < 0 || g > 255) {
-        return [-1, 'The value of G is out of range.', 0, 0, 0];
+        return CreateResponse(-1, 'The value of G is out of range.', 0, 0, 0);
     }
     if (isNaN(b)) {
-        return [-1, 'B is NaN.', 0, 0, 0];
+        return CreateResponse(-1, 'B is NaN.', 0, 0, 0);
     }
     if (b < 0 || b > 255) {
-        return [-1, 'The value of B is out of range.', 0, 0, 0];
+        return CreateResponse(-1, 'The value of B is out of range.', 0, 0, 0);
     }
 
     //OK
-    return [1, '', r, g, b];
+    return CreateResponse(1, '', r, g, b);
 }
 
 module.exports = checkColorValue;
