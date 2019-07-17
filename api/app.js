4bad2744fedfbde61c140b4d9382dad412ea2955
すー <s1732087AP@s.chibakoudai.jp>
2019-07-17 14:17:34 +0900

    Fix; typo

--- api/app.js
+++ api/app.js
@@ -36,7 +36,7 @@ app.use(function (req, res, next) {
     res.status(404).json({
         status:404,
         message:'Not Found',
-        Response:''
+        response:''
     });
     // next(createError(404));
 });
