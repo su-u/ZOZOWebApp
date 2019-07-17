da2fbca519e580b64fa87407e17a001eed050477
すー <s1732087AP@s.chibakoudai.jp>
2019-07-17 18:48:20 +0900

    Delete checkTypeValue.js

--- api/app/checkTypeValue.js
+++
@@ -1,9 +0,0 @@
-const checkTypeValue = (type) => {
-    if (type) {
-        return [1, '', type];
-    } else {
-        return [-1, 'type is empty.', ''];
-    }
-}
-
-module.exports = checkTypeValue;
